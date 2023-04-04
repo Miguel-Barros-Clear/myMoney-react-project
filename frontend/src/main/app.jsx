d673baf661a023077396f90e772ea7d497ad055b
Miguel-Barros-Clear <miguel.barros.clear@gmail.com>
2023-04-04 08:56:25 -0300

    feat: creating header component

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,6 +1,12 @@
 import "../common/template/dependecies";
 import React from "react";
 
+import Header from "../common/template/header";
+
 export default function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
