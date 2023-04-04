8cc462fecb3fa9bc1a8bd7a39f0efe1ad710f7ce
Miguel-Barros-Clear <miguel.barros.clear@gmail.com>
2023-04-04 09:47:57 -0300

    feat: creating dashboard components and row, grid and value box components

--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -3,12 +3,39 @@ import React, { Component } from "react";
 import ContentHeader from "../common/template/contentHeader";
 import Content from "../common/template/content";
 
+import Valuebox from "../common/widget/valuebox";
+import Row from "../common/layout/row";
+
 export default class Dashboard extends Component {
   render() {
     return (
       <div>
         <ContentHeader title="Dashboard" small="Versão 1.0" />
-        <Content>Dashboard</Content>
+        <Content>
+          <Row>
+            <Valuebox
+              cols="12 4"
+              color="green"
+              icon="bank"
+              value="R$ 10"
+              text="Total de Creditos"
+            />
+            <Valuebox
+              cols="12 4"
+              color="red"
+              icon="credit-card"
+              value="R$ 10"
+              text="Total de Debitos"
+            />
+            <Valuebox
+              cols="12 4"
+              color="blue"
+              icon="money"
+              value="R$ 10"
+              text="Valor Consolidado"
+            />
+          </Row>
+        </Content>
       </div>
     );
   }
