c3f0b68787b27d545580677d5900b1a33ebfba72
Miguel-Barros-Clear <miguel.barros.clear@gmail.com>
2023-04-04 10:51:06 -0300

    feat: creating tabheader component

--- frontend/src/biilingCycle/biilingCycle.jsx
+++ frontend/src/biilingCycle/biilingCycle.jsx
@@ -1,5 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 
-export default function BiilingCycle(props) {
-  return <div>Ciclo de pagamentos</div>;
+import ContentHeader from "../common/template/contentHeader";
+import Content from "../common/template/content";
+
+import Tabs from "../common/tab/tabs";
+import TabsContent from "../common/tab/tabsContent";
+import TabsHeader from "../common/tab/tabsHeader";
+import TabHeader from "../common/tab/tabHeader";
+
+export default class BiilingCycle extends Component {
+  render() {
+    return (
+      <div>
+        <ContentHeader title="Ciclos de Pagamentos" small="Cadastro" />
+        <Content>
+          <Tabs>
+            <TabsHeader>
+              <TabHeader label="Listar" icon="bars" target="tablist" />
+              <TabHeader label="Incluir" icon="plus" target="tabCreate" />
+              <TabHeader label="Alterar" icon="pencil" target="tabUpdate" />
+              <TabHeader label="Excluir" icon="trash-o" target="tabDelete" />
+            </TabsHeader>
+            <TabsContent></TabsContent>
+          </Tabs>
+        </Content>
+      </div>
+    );
+  }
 }
